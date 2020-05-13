d813b3be622254fd81b29692a0aa19a73f864862
edwardlanto <edwardlanto@hotmail.com>
2020-05-13 19:55:03 -0400

    lowercased all countries

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import CardDeck from 'react-bootstrap/CardDeck';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import "./index.css";
+import Columns from 'react-columns';
+import Form from 'react-bootstrap/Form';
 
 function App() {
   useEffect(() => {
@@ -13,27 +15,45 @@ function App() {
         axios.get('https://corona.lmao.ninja/v2/countries')
       ])
       .then((res) => {
+
+        // Lowercasing all countries
+        for (var i = 0; i < res[1].data.length; i++) {
+          res[1].data[i].country = res[1].data[i].country.toLowerCase();
+        }
+        console.log(res[1].data);
         setLatest(res[0].data);
-        setResults(res[1].data)
+        setResults(res[1].data);
       })
       .catch((err) => {
-        console.log(err);
+        setErr(err);
       });
   });
-  const [latest, setLatest] = useState("");
-  const [results, setResults] = useState([])
+
+  const [latest, setLatest] = useState([]);
+  const [results, setResults] = useState([]);
   const date = new Date(parseInt(latest.updated));
   const lastUpdated = date.toString();
+  const [searchCountries, setSearchCountries] = useState("");
+  const [err, setErr] = useState("");
 
-  const countries = results.map(data => (
+  const filterCountries = results.filter(item => {
+    return item.country.includes(searchCountries)
+  });
+
+  const sortByName = () => {
+    console.log(countries);
+  }
+
+  const countries = filterCountries.map((data, i) => (
     <Card
       bg="light"
       text="dark"
       className="text-center"
+      key={i}
       style={{ margin: "10px" }}
     >
       <Card.Body>
-        <Card.Img variant="top" src={data.countryInfo.flag} />
+        <Card.Img variant="top" src={data.countryInfo.flag} className="flag" />
         <Card.Body>
           <Card.Title>{data.country}</Card.Title>
           <Card.Text>Cases {data.cases}</Card.Text>
@@ -47,6 +67,14 @@ function App() {
       </Card.Body>
     </Card>
   ));
+
+  var queries = [{
+    columns: 2,
+    query: 'min-width:500px'
+  }, {
+    columns: 3,
+    query: 'min-width:1000px'
+  }];
   return (
     <div className="App">
       <CardDeck>
@@ -81,7 +109,28 @@ function App() {
           </Card.Footer>
         </Card>
       </CardDeck>
-      {countries}
+      <Form>
+        <Form.Group controlId="formGroupSearch">
+          <Form.Control
+            type="text"
+            placeholder="Search a country"
+            onChange={e => setSearchCountries(e.target.value)}
+          />
+        </Form.Group>
+      </Form>
+      <div className="text-center">
+        <button onClick={() => sortByName()}>Sort By Name</button>
+      </div>
+      {err ? <div>{err}</div> : ""}
+      {
+        countries.length > 0 ?
+          <Columns queries={queries}>
+            {countries}
+          </Columns>
+          : <div>Loading</div>
+      }
+
+
     </div>
   );
 }
